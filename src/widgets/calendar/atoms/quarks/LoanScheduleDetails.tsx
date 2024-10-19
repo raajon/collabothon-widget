4a3f68b9861da105f26fe9947d15e076eee875af
Agnieszka Iskierka <agnieszka.iskierka@gmail.com>
2024-10-19 19:59:26 +0200

    Event details

--- src/widgets/calendar/atoms/quarks/LoanScheduleDetails.tsx
+++ src/widgets/calendar/atoms/quarks/LoanScheduleDetails.tsx
@@ -3,23 +3,22 @@ import { LoanScheduleProps } from "../../../../lib/types";
 import { Col, Row, Statistic } from "antd";
 
 const LoanScheduleDetails = ({ data }: Props) => {
-  console.log(data);
   return (
     <>
       <Row gutter={16}>
         <Col span={12}>
           <Statistic title="Loan" value={data.loanDetails.title} valueStyle={{fontSize: '20px'}}/>
         </Col>
-        <Col span={12}>
-          <Statistic title="Currency" value={data.currency} valueStyle={{fontSize: '20px'}}/>
-        </Col>
       </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Statistic title="Capital installement" value={data.capital_installement} valueStyle={{fontSize: '20px'}}/>
+      <Row gutter={16} style={{marginTop: 28,}}>
+        <Col span={8}>
+          <Statistic title="Capital installement" value={data.capital_installement} valueStyle={{fontSize: '25px'}}/>
         </Col>
-        <Col span={12}>
-          <Statistic title="Interest installement" value={data.interest_installement} valueStyle={{fontSize: '20px'}}/>
+        <Col span={8}>
+          <Statistic title="Interest installement" value={data.interest_installement} valueStyle={{fontSize: '25px'}}/>
+        </Col>
+        <Col span={8}>
+          <Statistic title="Currency" value={data.currency} valueStyle={{fontSize: '25px'}}/>
         </Col>
       </Row>
     </>
--- src/widgets/calendar/atoms/quarks/StandingOrderDetails.tsx
+++ src/widgets/calendar/atoms/quarks/StandingOrderDetails.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { StandingOrderProps } from "../../../../lib/types";
+import { Col, Row, Statistic } from "antd";
 
 const StandingOrderDetails = ({ data }: Props) => {
-  return <p>{data.currency}</p>;
+  return (
+    <>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Statistic title="Account from" value={data.account_from} valueStyle={{fontSize: '20px'}}/>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{marginTop: 20,}}>
+        <Col span={12}>
+          <Statistic title="Amount" value={data.amount} valueStyle={{fontSize: '25px'}}/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Currency" value={data.currency} valueStyle={{fontSize: '25px'}}/>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{marginTop: 20,}}>
+        <Col span={12}>
+          <Statistic title="Account to" value={data.account_to} valueStyle={{fontSize: '20px'}}/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Receiver" value={data.receiver} valueStyle={{fontSize: '20px'}}/>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 interface Props {
